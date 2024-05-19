5077e41bc6629814c60dfb12a34782b96e046664
Pramatya Jati <35680922+pramjati02@users.noreply.github.com>
2024-05-19 14:14:12 +0200

    Update results_resnet50.py

--- Resnet50/results_resnet50.py
+++ Resnet50/results_resnet50.py
@@ -10,6 +10,7 @@ import tensorflow as tf
 from sklearn.metrics import f1_score, classification_report
 from keras import backend as K
 
+#  adapted from https://datascience.stackexchange.com/questions/45165/how-to-get-accuracy-f1-precision-and-recall-for-a-keras-model
 def recall_m(y_true, y_pred): # suitable only for binary classification?
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
